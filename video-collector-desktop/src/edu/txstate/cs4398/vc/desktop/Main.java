faa5c040bafc0fc7a24dc64e19177abb4c5e326d
Edward Thorne <Ed@TXSTATEVM>
2012-08-05 13:14:04 -0500

    Use Native OS look and feel.

--- video-collector-desktop/src/edu/txstate/cs4398/vc/desktop/Main.java
+++ video-collector-desktop/src/edu/txstate/cs4398/vc/desktop/Main.java
@@ -3,11 +3,13 @@
  */
 package edu.txstate.cs4398.vc.desktop;
 
+import javax.swing.UIManager;
+
 import edu.txstate.cs4398.vc.desktop.controller.CollectorController;
 
 /**
  * @author Ed
- *
+ * 
  */
 public class Main {
 
@@ -15,6 +17,14 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// use the native OS look and feel
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			// eat the exceptions since getSystemLookAndFeelClassName should
+			// always be a valid PLAF choice
+		}
+
 		// start the application by creating a controller
 		new CollectorController();
 	}
